7ebb16a9accd20ded6287ae0dd362b59eac2e662
Praveen.ravva61 <praveen.ravva61@gmail.com>
2026-03-12 09:35:00 +0530

    hierarchcial aget created

--- agent.py
+++ agent.py
@@ -1,3 +1,20 @@
 from google.adk.agents import Agent, ParallelAgent, SequetialAgent, LllAgent
 from google.adk.tools import google_search
 
+#==================================================================================
+# Hierarchical Agent
+#==================================================================================
+# In a complex scenario, we may need orchestrator agent that plans automously and
+# delegates tasks to sub-agents. The orchestrator agent can be implemented as a hierarchical agent.
+
+# It uses "Agent tool" concept. Manager treats the planer as tool to get the plan,
+# then activates sub-agents to execute it.
+
+from google.adk.tools.agent_tool import AgentTool
+from google.adk.apps import App
+
+
+#==================================================================================
+# Planer Agent
+#==================================================================================
+
